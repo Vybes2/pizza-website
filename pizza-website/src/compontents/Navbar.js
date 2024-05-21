669ad8db9e0b58328c1ab9bff7e2decf92f8b287
Vybes2 <oganthony82@gmail.com>
2024-05-21 13:41:56 -0400

    Update Navbar.js

--- pizza-website/src/compontents/Navbar.js
+++ pizza-website/src/compontents/Navbar.js
@@ -3,6 +3,14 @@ import React from 'react'
 function Navbar() {
   return (
     <>
+      <div className="navbar">
+        <div className="navbar-container container">
+          <link to="/" className='navbar-logo'>
+            PIZZA
+          </link>
+          
+        </div>
+      </div>
     </>
   )
 }
